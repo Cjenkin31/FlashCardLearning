b9bfcb6f0102a6256d58d439beec6d23b2c9b391
Cjenkin31 <christopherjenkin64@gmail.com>
2023-01-30 12:03:08 -0500

    encoded the flashcards

--- FlashCardReader.py
+++ FlashCardReader.py
@@ -8,7 +8,7 @@ def TermDefDict():
     except (OSError, IOError) as e:
         with open(r"allCards", 'r') as cardFile:
             for line in cardFile:
-                x = line[:-1]
+                x = line[:-1].encode('utf-8')[2:-1]
                 termsDefin[x.split(',')[0]] = x.split(',')[1]
         pickle.dump(termsDefin, open("dict.pickle", "wb"))
     return(termsDefin)
